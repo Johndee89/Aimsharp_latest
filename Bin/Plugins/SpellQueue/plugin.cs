447911920c6fa4062c220270a2f04d5e51f270fd
Johndee89 <jdebure@gmail.com>
2024-07-21 12:44:11 +0200

    first commit

---
+++ Bin/Plugins/SpellQueue/plugin.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AimsharpWow.API; //needed to access Aimsharp API
+
+namespace AimsharpWow.Modules
+{
+    public class SpellQueue : Plugin
+    {
+
+        public override void Initialize()
+        {
+
+            Aimsharp.PrintMessage("Spell Queue Plugin");
+            Aimsharp.PrintMessage("Use this macro to manually queue spells: /xxxxx queue spellname");
+
+            
+        }
+
+        public override bool CombatTick()
+        {
+            
+            bool IAmChanneling = Aimsharp.IsChanneling("player");
+            int GCD = Aimsharp.GCD();
+            bool LineOfSighted = Aimsharp.LineOfSighted();
+
+            bool BuffGhostWolfUp = Aimsharp.HasBuff("Ghost Wolf");
+
+            string QueuedSpell = Aimsharp.GetSpellQueue();
+
+            Aimsharp.PrintMessage("Queued spell is: " + QueuedSpell);
+
+            if (QueuedSpell == "Thunderstorm")
+            {
+                if (Aimsharp.CanCast("Thunderstorm","player"))
+                {
+                    Aimsharp.Cast("ThunderstormC");
+                    return true;
+                }
+            }
+            else if (QueuedSpell == "Earthquake")
+            {
+                if (Aimsharp.CanCast("Earthquake","player"))
+                {
+                    Aimsharp.Cast("EarthquakeC");
+                    return true;
+                }
+            }
+            else if (QueuedSpell == "Earthbind Totem")
+            {
+                if (Aimsharp.CanCast("Earthbind Totem", "player"))
+                {
+                    Aimsharp.Cast("EB");
+                    return true;
+                }
+            }
+            else if (QueuedSpell == "Capacitor Totem")
+            {
+                if (Aimsharp.CanCast("Capacitor Totem", "player"))
+                {
+                    Aimsharp.Cast("Cap");
+                    return true;
+                }
+            }
+            else if (QueuedSpell == "Totemic Projection")
+            {
+                if (Aimsharp.CanCast("Static Field Totem", "player"))
+                {
+                    Aimsharp.Cast("SFT");
+                    return true;
+                }
+                if (GCD <= 500)
+                {
+                    if (Aimsharp.CanCast("Totemic Projection", "player"))
+                    {
+                        Aimsharp.Cast("TP");
+                        return true;
+                    }
+                }
+                return true;
+            }
+            else if (Aimsharp.CanCast(QueuedSpell,"player"))
+            {
+                Aimsharp.Cast(QueuedSpell);
+                return true;
+            }
+
+            return false;
+        }
+
+        public override bool OutOfCombatTick()
+        {
+            return CombatTick();
+
+        }
+    }
+}
---
+++ Legendary_loader.bat
@@ -0,0 +1,59 @@
+
+@echo off
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    set "PROCESS1_NAME=Battle.net*"
+	set "PROCESS2_NAME=Wow*"
+
+	echo Checking for Battle.net...
+	tasklist /NH /FI "IMAGENAME eq %PROCESS1_NAME%" | find /I "Battle.net" >nul
+	if errorlevel 1 (
+		echo Battle.net is not running.
+	) else (
+		echo Found Battle.net. Terminating process...
+		for /f "tokens=2" %%i in ('tasklist /NH /FI "IMAGENAME eq %PROCESS1_NAME%"') do taskkill /F /IM "%%i" /T
+	)
+
+	echo Checking for World of Warcraft...
+	tasklist /NH /FI "IMAGENAME eq %PROCESS2_NAME%" | find /I "WoW" >nul
+	if errorlevel 1 (
+		echo World of Warcraft is not running.
+	) else (
+		echo Found World of Warcraft. Terminating process...
+		for /f "tokens=2" %%i in ('tasklist /NH /FI "IMAGENAME eq %PROCESS2_NAME%"') do taskkill /F /IM "%%i" /T
+	)
+    pushd "%CD%"
+    CD /D "%~dp0"
+    CD Bin
+
+
+    pause
+
+    echo --Loading AimsharpWow bot...--
+    START AimsharpWow.exe
+
+    echo --Work finished.--
+
+    pause
+
+:--------------------------------------
